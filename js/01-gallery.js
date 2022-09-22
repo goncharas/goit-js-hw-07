1f7477c1dab5a66bb4ee9f1e8dd5a86ea523f475
Andrii Honchar <goncharas1988@gmail.com>
2022-09-22 19:54:57 +0300

    add task 1 and task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
 console.log(galleryItems);
+const imageConteiner = document.querySelector(".gallery");
+const imageMarkup = createGallery(galleryItems);
+imageConteiner.insertAdjacentHTML("beforeend", imageMarkup);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      rel="lightbox"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+imageConteiner.addEventListener("click", onClickImage);
+
+function onClickImage(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  const clickImg = evt.target.dataset.source;
+  // console.dir(evt.target);
+  const instance = basicLightbox.create(`<img src = "${clickImg}" />`, {
+    onShow: (instance) => {
+      window.addEventListener("keydown", keyClose);
+    },
+    onClose: (instance) => {
+      window.removeEventListener("keydown", keyClose);
+    },
+  });
+  instance.show();
+
+  function keyClose(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  }
+}
